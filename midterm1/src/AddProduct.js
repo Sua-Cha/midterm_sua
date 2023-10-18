36041dbe5c5425729bb7da6dc0e18b07b9d36ffa
Sua-Cha <chasua.gg@gmail.com>
2023-10-17 20:45:17 -0400

    app.js

---
+++ midterm1/src/AddProduct.js
@@ -0,0 +1,147 @@
+import React, { useState } from 'react';
+
+ 
+
+const AddProduct = () => {
+
+    const [formData, setFormData] = useState({
+
+        Name: '',
+
+        Description: '',
+
+        Category: '',
+
+        Quantity: '',
+
+        Price: '',
+
+    });
+
+ 
+
+    const handleInputChange = (event) => {
+
+        const { name, value } = event.target;
+
+        setFormData({ ...formData, [name]: value });
+
+    };
+
+ 
+
+    const handleSubmit = (event) => {
+
+        event.preventDefault();
+
+        //console.log(formData);
+
+        alert(JSON.stringify(formData));
+
+    };
+
+ 
+
+    return (
+
+        <div>
+
+            <form onSubmit={handleSubmit}>
+
+                <input
+
+                    type="text"
+
+                    name="Name"
+
+                    placeholder="Name"
+
+                    value={formData.Name}
+
+                    onChange={handleInputChange}
+
+                />
+
+                <br />
+
+                <input
+
+                    type="text"
+
+                    name="Description"
+
+                    placeholder="Description"
+
+                    value={formData.Description}
+
+                    onChange={handleInputChange}
+
+                />
+
+                <br />
+
+                <input
+
+                    type="text"
+
+                    name="Category"
+
+                    placeholder="Category"
+
+                    value={formData.Category}
+
+                    onChange={handleInputChange}
+
+                />
+
+                <br />
+
+                <input
+
+                    type="text"
+
+                    name="Quantity"
+
+                    placeholder="Quantity"
+
+                    value={formData.Quantity}
+
+                    onChange={handleInputChange}
+
+                />
+
+                <br />
+
+                <input
+
+                    type="text"
+
+                    name="Price"
+
+                    placeholder="Price"
+
+                    value={formData.Price}
+
+                    onChange={handleInputChange}
+
+                />
+
+                <br />
+
+                <button type="submit" class="success">Submit</button>
+
+                <button type="submit" class="cancel">Cancel</button>
+
+            </form>
+
+        </div>
+
+    );
+
+};
+
+ 
+
+export default AddProduct;
+
+ 
